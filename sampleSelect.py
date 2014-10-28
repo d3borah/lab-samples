4a84c23ae11470fb8ed40be1feade69b9619d7a6
D3borah <deborah.siegel@gmail.com>
2014-10-28 16:55:17 -0700

    sorted output

--- sampleSelect.py
+++ sampleSelect.py
@@ -1,30 +1,47 @@
+"""
+Python script to select one replacement sample for each of the patients
+who have been "reset" for sequencing
+"""
+
 import sys
 from collections import defaultdict
 
 filename = "samples.csv"
 
-with open(filename) as f:content = f.readlines()[1:] #skip the header
+#read the file lines, skipping the header
+with open(filename) as f:
+    content = f.readlines()[1:] 
+
+#create empty list because split returns a list
 data = []
 
+
+#split each line in a list
 for line in content: 
-    data.append(line.strip().split(',')) #split returns a list, if you want a tuple, convert it to a tuple
+    data.append(line.strip().split(','))
 
 #print data[0]
 
-patient_ids = [x[7] for x in data_list]#list comprehension
+#slice the list into new list in a list comprehension
+patient_ids = [x[7] for x in data_list]
 len(patient_ids)
 unique_patient_ids = set(patient_ids)
 len(unique_patient_ids)
+
+#The defaultdict will create any items that you try to access (provided of course they do not exist yet). the values are lists. 
 patientSamples = defaultdict(list)
 
+#make the per patient dictionary containing all of that patient's samples
 for patient in unique_patient_ids:
     for sample in data:
         if sample[7] == patient:
                 patientSamples[patient].append(sample)
 
+
 #for key, value in patientSamples.iteritems():
     #print key, 'corresponds to',patientSamples[key]
 
+#make a list to contain the patients we want to find the additional samples for
 chosenPatients = []
 for key, value in patientSamples.iteritems():
     for myvalue in patientSamples[key]:
@@ -33,6 +50,10 @@ for key, value in patientSamples.iteritems():
         
 len(chosenPatients)
 
+"""
+go through the samples and select just one sample for each of the chosen patients, provided 
+the sample status is "Duplicate" or blank
+"""
 chosenSamples = {}
 for patient in chosenPatients:
     if patient in chosenSamples:
@@ -43,14 +64,20 @@ for patient in chosenPatients:
                 if ((myvalue[5] == "") | (myvalue[5] == "Duplicate")):
                     chosenSamples[patient] = myvalue            
 
-len(chosenSamples)
 
-#for item in chosenSamples:
-    #print chosenSamples[item][5]
+chosenSampList = []
+for item in chosenSamples:
+    chosenSampList.append(chosenSamples[item])
+
+
+import operator
+chosenSampList.sort(key=operator.itemgetter(8))
 
 f = open('sample_outputfile_backup.csv', 'w')
 
 #emit results
-for item in chosenSamples:
-    f.write(', '.join(chosenSamples[item])  )
+for s in chosenSampList:       
+    f.write(', '.join(s) )
     f.write("\n")
+
+
